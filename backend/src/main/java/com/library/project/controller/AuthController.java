3628793d6e98750af147b0bf395f012b9e6489da
Ravi narayana Brahma <165268765+Ravi-narayana-brahma@users.noreply.github.com>
2026-03-06 13:16:27 +0530

    Update AuthController.java

--- backend/src/main/java/com/library/project/controller/AuthController.java
+++ backend/src/main/java/com/library/project/controller/AuthController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.library.project.service.AuthService;
 
 import jakarta.servlet.http.HttpSession;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/auth")
 public class AuthController {
 
