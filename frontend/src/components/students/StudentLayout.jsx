9499862ca7a3196415be7c26a957beed8c5b1712
Ravi narayana Brahma <165268765+Ravi-narayana-brahma@users.noreply.github.com>
2026-03-07 18:58:46 +0530

    Update StudentLayout.jsx

--- frontend/src/components/students/StudentLayout.jsx
+++ frontend/src/components/students/StudentLayout.jsx
@@ -252,14 +252,15 @@ useEffect(() => {
 
                   <div>
 
-                 <p className="notif-title"
-                      {n.request_type === "ISSUE" && n.status === "APPROVED" && "📗 Book Issued"}
-                      {n.request_type === "RETURN" && n.status === "APPROVED" && "🔁 Return Approved"}
-                    
-                      {n.request_type === "ISSUE" && n.status === "REJECTED" && "❌ Issue Request Rejected"}
-                      {n.request_type === "RETURN" && n.status === "REJECTED" && "❌ Return Request Rejected"}
-                    
-                    </p>
+                <p className="notif-title">
+
+                    {n.request_type === "ISSUE" && n.status === "APPROVED" && "📗 Book Issued"}
+                    {n.request_type === "RETURN" && n.status === "APPROVED" && "🔁 Return Approved"}
+                  
+                    {n.request_type === "ISSUE" && n.status === "REJECTED" && "❌ Issue Request Rejected"}
+                    {n.request_type === "RETURN" && n.status === "REJECTED" && "❌ Return Request Rejected"}
+                  
+                  </p>
                     <span className="notif-msg">
                       {n.message}
                     </span>
