8b39e9f285058d9021fabc97cc58b729f864b306
Ravi narayana Brahma <165268765+Ravi-narayana-brahma@users.noreply.github.com>
2026-03-09 16:37:23 +0530

    Update LibraryController.java

--- backend/src/main/java/com/library/project/controller/LibraryController.java
+++ backend/src/main/java/com/library/project/controller/LibraryController.java
@@ -200,13 +200,13 @@ public class LibraryController {
     // Mark copy status
     @PostMapping("/copy/mark-status")
     public ResponseEntity<?> markCopyStatus(
-            @RequestParam Long copyId,
+            @RequestParam String copyCode,
             @RequestParam String status,
 			@RequestParam double fine) {
         try {
             return ResponseEntity.ok(
                     libraryService
-                            .markCopyStatus(copyId, status, fine));
+                            .markCopyStatusByCode(copyId, status, fine));
         } catch (Exception e) {
             return ResponseEntity
                     .badRequest()
