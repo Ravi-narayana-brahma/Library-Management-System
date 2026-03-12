e79b052668f68eae81e94666dde376c2482faa73
Ravi narayana Brahma <165268765+Ravi-narayana-brahma@users.noreply.github.com>
2026-03-12 15:48:14 +0530

    Update LibraryController.java

--- backend/src/main/java/com/library/project/controller/LibraryController.java
+++ backend/src/main/java/com/library/project/controller/LibraryController.java
@@ -8,7 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.library.project.entity.*;
 import com.library.project.service.LibraryService;
 
